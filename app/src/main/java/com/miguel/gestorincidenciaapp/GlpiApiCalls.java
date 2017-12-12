f624ef0f4fb7b58edd11d77379b85948f760cf29
Miguel Angel Lopez <mlopezreynaldo@gmail.com>
2017-12-12 21:17:31 +0100

    Prepare Pojo and Interface Api calls

---
+++ app/src/main/java/com/miguel/gestorincidenciaapp/GlpiApiCalls.java
@@ -0,0 +1,23 @@
+package com.miguel.gestorincidenciaapp;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
+/**
+ * Created by 23895346w on 12/12/17.
+ */
+
+public interface GlpiApiCalls {
+
+//    @GET("http://5.145.175.176/glpi/apirest.php/initSession?login=glpi&password=D1A2I3")
+//    @Header("App-Token=5o9yiRFgOUlOVYxZLnF1taKj67lnW4bSDUXGUlAj")
+
+//    @GET("http://5.145.175.176/glpi/apirest.php/getFullSession")
+//    @Header("App-Token=5o9yiRFgOUlOVYxZLnF1taKj67lnW4bSDUXGUlAj, Session-Token=rl9u8odq13hehr881mk979usg5")
+
+
+
+}
